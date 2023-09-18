d9eb78011e80fe1b4527e8b652f0c8e873587125
clemensolana <clementesolana@outlook.com>
2023-09-18 18:02:13 -0300

    incorporo multer al proyecto para adjuntar imagenes de perfil a la hora de registrar un usuario

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../../public/images/profilePictures'));
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    const newFileName =
+      'profilePicture-' + Date.now() + path.extname(file.originalname);
+    cb(null, newFileName);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const usersRouter = express.Router();
 const usersController = require('../controllers/usersController.js');
 
 usersRouter.get('/register', usersController.register);
 
-usersRouter.post('/register', usersController.create);
+usersRouter.post(
+  '/register',
+  upload.single('profilePicture'),
+  usersController.create
+);
 
 usersRouter.get('/login', usersController.login);
 
